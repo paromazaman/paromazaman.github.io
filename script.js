b8f3029c0068f5ec916b36b69274fcd4f64f1801
paromazaman <146149540+paromazaman@users.noreply.github.com>
2025-01-16 03:10:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // This script can handle additional interactivity in the future if needed.//
 
+// Get all gallery images
+const galleryImages = document.querySelectorAll('.gallery-image');
+
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        // Toggle the 'enlarged' class when the image is clicked
+        image.classList.toggle('enlarged');
+    });
+});
