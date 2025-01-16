7263900ff668ed74c8caaeaebd961fc8f1867e8b
paromazaman <146149540+paromazaman@users.noreply.github.com>
2025-01-15 21:32:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 // This script can handle additional interactivity in the future if needed.//
-// JavaScript for handling hover effects and image transition on hover
-document.querySelectorAll('.header-image').forEach((image) => {
-    image.addEventListener('mouseover', () => {
-        image.style.opacity = '0';
-    });
-    image.addEventListener('mouseout', () => {
-        image.style.opacity = '1';
-    });
-});
+
